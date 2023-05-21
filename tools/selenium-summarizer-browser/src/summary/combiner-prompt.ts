400d8eeeb95e6afa864cab4872f5562f7eb9c318
Altaflux <pabloloz@gmail.com>
2023-05-20 17:44:09 -0700

    Change prompt to a combiner

---
+++ tools/selenium-summarizer-browser/src/summary/combiner-prompt.ts
@@ -0,0 +1,33 @@
+
+
+import {
+    PromptTemplate,
+} from "langchain/prompts";
+
+
+
+const _DEFAULT_SUMMARIZER_TEMPLATE2 = `Combine the following two pieces of a markdown documents into one.": 
+The result should be in Markdown format.
+Prioritize content related to the following: "{focus}".
+
+Include the html inputs, buttons and link elements that are relevant (always including all attributes and text). Do not rewrite or simplify the html elements!
+
+If the combined document is too long, remove content that is not related to the focus.
+
+-------------------------
+This is part 1 of the document:
+
+{document1}
+
+-------------------------
+This is part 2 of the document:
+
+{document2}
+
+-------------------------
+Combined Markdown document:
+`;
+export const COMBINE_PROMPT = /* #__PURE__ */ new PromptTemplate({
+    template: _DEFAULT_SUMMARIZER_TEMPLATE2,
+    inputVariables: ["document1", "document2", "focus"],
+});
