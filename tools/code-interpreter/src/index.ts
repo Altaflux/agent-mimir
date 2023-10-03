9e975827331be4a948e920f260df74d53cfcbeed
Pablo Lozano <4032486+Altaflux@users.noreply.github.com>
2023-10-03 13:59:35 -0700

    more changes

--- tools/code-interpreter/src/index.ts
+++ tools/code-interpreter/src/index.ts
@@ -27,41 +27,41 @@ If you need to return files to the human when using the code-interpreter, please
 End of Code Interpreter Functions Instructions.
 
 `;
-// export class CodeInterpreterPlugin  {
+export class CodeInterpreterPlugin  {
 
-// }
-export class CodeInterpreterPlugin extends MimirAgentPlugin {
-    constructor() {
-        super();
-    }
-
-    systemMessages(): (SystemMessagePromptTemplate | MessagesPlaceholder)[] {
-        return [
-            SystemMessagePromptTemplate.fromTemplate(PROMPT),
-        ];
-    }
-
-    async getInputs(): Promise<Record<string, any>> {
-        const files = await fs.readdir("C:/AI/interpreter/in");
-        if (files.length === 0) {
-            return {
-                interpreterInputFiles: "",
-            };
-        }
-        const bulletedFiles = (files)
-            .map((fileName: string) => `- "${fileName}"`)
-            .join("\n");
-        return {
-            interpreterInputFiles: `The human has given you access to the following files for you to use with the code interpreter functions. The files can be found inside a directory path saved in the variable named "inputDirectory". :\n${bulletedFiles}\n`,
-        };
-    }
-
-    tools() {
-        return [
-            new PythonCodeInterpreter("C:/AI/interpreter/"),
-        ];
-    };
 }
+// export class CodeInterpreterPlugin extends MimirAgentPlugin {
+//     constructor() {
+//         super();
+//     }
+
+//     systemMessages(): (SystemMessagePromptTemplate | MessagesPlaceholder)[] {
+//         return [
+//             SystemMessagePromptTemplate.fromTemplate(PROMPT),
+//         ];
+//     }
+
+//     async getInputs(): Promise<Record<string, any>> {
+//         const files = await fs.readdir("C:/AI/interpreter/in");
+//         if (files.length === 0) {
+//             return {
+//                 interpreterInputFiles: "",
+//             };
+//         }
+//         const bulletedFiles = (files)
+//             .map((fileName: string) => `- "${fileName}"`)
+//             .join("\n");
+//         return {
+//             interpreterInputFiles: `The human has given you access to the following files for you to use with the code interpreter functions. The files can be found inside a directory path saved in the variable named "inputDirectory". :\n${bulletedFiles}\n`,
+//         };
+//     }
+
+//     tools() {
+//         return [
+//             new PythonCodeInterpreter("C:/AI/interpreter/"),
+//         ];
+//     };
+// }
 
 class PythonCodeInterpreter extends StructuredTool {
 
