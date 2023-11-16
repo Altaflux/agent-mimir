c280356a22dc0a899f10ba8b1f3cee28111ef8ae
Pablo Lozano <4032486+Altaflux@users.noreply.github.com>
2023-11-15 20:52:12 -0800

    Fix message compaction.

--- agent-mimir/src/utils/format.ts
+++ agent-mimir/src/utils/format.ts
@@ -1,7 +1,7 @@
-import { BaseMessage, ChatMessage } from "langchain/schema";
+import { BaseMessage, ChatMessage, MessageContent } from "langchain/schema";
 
 
-// TODO ARREGLAR ESTO., el content es
+
 export function messagesToString(
   messages: BaseMessage[],
   humanPrefix = "Human",
@@ -12,27 +12,37 @@ export function messagesToString(
     let role: string;
     if (m._getType() === "human") {
       role = humanPrefix;
-      const mm = `Start-Of-Message:\n- Participant: ${role}\n- Task: message\n- Payload: ${m.content}`
+      const mm = `Start-Of-Message:\n- Participant: ${role}\n- Task: message\n- Payload: ${extractTextContent(m.content)}`
       string_messages.push(mm);
     } else if (m._getType() === "ai") {
       role = aiPrefix;
       const functionName = m.additional_kwargs.function_call?.name ?? "message";
-      const arg = m.additional_kwargs.function_call?.arguments ?? m.content;
+      const arg = m.additional_kwargs.function_call?.arguments ?? extractTextContent(m.content);
       const mm = `Start-Of-Message:\n- Participant: ${role}\n- Task: ${functionName}\n- Payload: ${arg}`
       string_messages.push(mm);
     } else if (m._getType() === "system") {
       role = humanPrefix;
-      string_messages.push(`${role}: ${m.content}`);
+      string_messages.push(`${role}: ${extractTextContent(m.content)}`);
     } else if (m._getType() === "function") {
       role = humanPrefix;
-      const mm = `Start-Of-Message:\n- Participant: ${role}\n- Task: message\n- Payload: The "${m.name}" tool responded the following: ${m.content}`
+      const mm = `Start-Of-Message:\n- Participant: ${role}\n- Task: message\n- Payload: The "${m.name}" tool responded the following: ${extractTextContent(m.content)}`
       string_messages.push(mm);
     } else if (m._getType() === "generic") {
       role = humanPrefix;
-      string_messages.push(`${role}: ${m.content}`);
+      string_messages.push(`${role}: ${extractTextContent(m.content)}`);
     } else {
       throw new Error(`Got unsupported message type: ${m}`);
     }
   }
   return string_messages.join("\n\n\n");
+}
+
+function extractTextContent(messageContent: MessageContent) {
+  if (typeof messageContent === "string") {
+    return messageContent;
+  } else if (Array.isArray(messageContent)) {
+    return (messageContent as any).find((e: any) => e.type === "text")?.text ?? "";
+  } else {
+    throw new Error(`Got unsupported text type: ${JSON.stringify(messageContent)}`);
+  }
 }
