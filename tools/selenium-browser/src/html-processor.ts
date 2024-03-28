c6e20f2f0cc6970db3d12211b89e0cb83da7525f
Pablo Lozano <4032486+Altaflux@users.noreply.github.com>
2024-03-27 22:57:29 -0700

    web browser performance improvement.

--- tools/selenium-browser/src/html-processor.ts
+++ tools/selenium-browser/src/html-processor.ts
@@ -1,6 +1,6 @@
 import { JSDOM } from 'jsdom';
 import { getXPath } from './xpath.js';
-import { By, WebDriver } from 'selenium-webdriver';
+import { By, WebDriver, WebElement } from 'selenium-webdriver';
 
 const selectableElements = ['input', 'select', 'textarea'];
 const clickableElements = ['a', 'button', 'link'];
@@ -58,83 +58,90 @@ function convertPicturesToImages(doc: Element) {
 async function findAllRelevantElements(doc: Element, driver: WebDriver) {
     const allElements = doc.querySelectorAll('*');
 
-    const htmlElementInformation = await Promise.all(Array.from(allElements)
+    const htmlElements = await Promise.all(Array.from(allElements)
         .filter((e) => isRelevantElement(e))
         .map(async (element) => {
             const tag = element.tagName.toLowerCase();
             const type = (selectableElements.includes(tag) ? 'input' : interactableElements.includes(tag) ? 'clickable' : 'text') as RelevantElement;
             const xpath = getXPath(element);
             const byExpression = By.xpath(xpath);
-            let location = {
-                top: 0,
-                left: 0,
-                isViewable: true,
-            };
-            try {
-                const webDriverElement = await driver!.findElement(byExpression);
-                location = await driver.executeScript(`
-                //window.document.documentElement.style.setProperty("scroll-behavior", "auto", "important");
-                function isElementUnderOverlay(element) {
-                    const rect = element.getBoundingClientRect();
-                    const topElement = document.elementFromPoint(rect.left + rect.width / 2, rect.top + rect.height / 2);
-                    return topElement !== element && !element.contains(topElement);
-                }
-                
-                function isElementClickable(element) {
-                    const styles = getComputedStyle(element);
-                    return styles.pointerEvents !== 'none';
-                }
-
-                const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
-                    const { top, left, bottom, right } = el.getBoundingClientRect();
-                    const { innerHeight, innerWidth } = window;
-                    const result = partiallyVisible
-                      ? ((top > 0 && top < innerHeight) ||
-                          (bottom > 0 && bottom < innerHeight)) &&
-                          ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
-                      : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
-                    return result;
-                };
-                
-                function getOffset(el) {
-                    const rect = el.getBoundingClientRect();
-                    return {
-                      left: rect.left,
-                      top: rect.top
-                    };
-                }
-                let element = arguments[0];
-                //element.scrollIntoView({ behavior: "instant", block: "center", inline: "nearest" });
-                const rect = getOffset(element);
-                return {
-                    top: rect.top,
-                    left: rect.left,
-                    isViewable: !isElementUnderOverlay(element) && isElementClickable(element) && elementIsVisibleInViewport(element)
-                };
-                `, webDriverElement);
-            } catch (e) {
-
-            }
+            const webDriverElement = await driver!.findElement(byExpression);
             return {
                 id: element.getAttribute('x-interactableId')!,
                 xpath: getXPath(element),
                 element: element,
                 type: type,
-                location: location,
+                webDriverElement: webDriverElement,
             }
         }));
-    for (const foundElement of htmlElementInformation) {
+
+    const htmlElementInformation: {
+        id: string, xpath: string, element: Element, webDriverElement: WebElement, type: RelevantElement, location: { top: number, left: number, isViewable: boolean }
+    }[] = await driver.executeScript(`
+            function isElementUnderOverlay(element) {
+                const rect = element.getBoundingClientRect();
+                const topElement = document.elementFromPoint(rect.left + rect.width / 2, rect.top + rect.height / 2);
+                return topElement !== element && !element.contains(topElement);
+            }
+            
+            function isElementClickable(element) {
+                const styles = getComputedStyle(element);
+                return styles.pointerEvents !== 'none';
+            }
+            
+            const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
+                const { top, left, bottom, right } = el.getBoundingClientRect();
+                const { innerHeight, innerWidth } = window;
+                const result = partiallyVisible
+                ? ((top > 0 && top < innerHeight) ||
+                    (bottom > 0 && bottom < innerHeight)) &&
+                    ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+                : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+                return result;
+            };
+            
+            function getOffset(el) {
+                const rect = el.getBoundingClientRect();
+                return {
+                left: rect.left,
+                top: rect.top
+                };
+            }
+            
+            
+            let elements = arguments[0];
+            
+            return elements.map((info) => {
+                const rect = getOffset(info.webDriverElement);
+                return {
+                    ...info,
+                    location: {
+                        top: rect.top,
+                        left: rect.left,
+                        isViewable: !isElementUnderOverlay(info.webDriverElement) && isElementClickable(info.webDriverElement) && elementIsVisibleInViewport(info.webDriverElement)
+                    }
+                };
+            });
+        `, htmlElements)
+    const elements = htmlElementInformation.map((el) => ({ ...el, element: doc.querySelector(`[x-interactableId="${el.id}"]`) }))
+
+    for (const foundElement of elements) {
         if (!foundElement.location.isViewable) {
+            //let elementToRemove = foundElement.element;
             let elementToRemove = foundElement.element;
             if (elementToRemove) {
-                elementToRemove.remove();
+                try {
+                    elementToRemove.remove();
+                } catch (ee) {
+                    console.error("Failed to remote, ", ee);
+                }
+
             }
         }
     }
-    return htmlElementInformation.filter((e) => e.location.isViewable);
+    return elements.filter((e) => e.location.isViewable);
 
 }
-
 
 
 export type InteractableElement = {
@@ -153,10 +160,10 @@ export async function extractHtml(html: string, driver: WebDriver) {
     let allRelevantElements = await findAllRelevantElements(cleanHtml, driver);
 
     let interactables = allRelevantElements
-        .filter((relevant) => interactableElements.includes(relevant.element.tagName.toLowerCase()))
+        .filter((relevant) => (relevant.element !== null) && interactableElements.includes(relevant.element.tagName.toLowerCase()))
         .map((entries) => {
             return {
-                id: entries.element.getAttribute('x-interactableId')!,
+                id: entries.id,
                 xpath: entries.xpath,
                 type: entries.type,
                 location: entries.location,
