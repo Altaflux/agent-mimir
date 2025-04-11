5181019797f1780710a26af9a6960c7e3f03b4cb
Pablo Lozano <4032486+Altaflux@users.noreply.github.com>
2025-04-11 11:39:57 -0700

    Refactor LocalPythonExecutor: Consolidate temporary directory management and improve virtual environment setup

--- agent-mimir/src/agent-manager/code-agent/executors/localExecutor.ts
+++ agent-mimir/src/agent-manager/code-agent/executors/localExecutor.ts
@@ -9,50 +9,60 @@ import { spawn } from 'child_process';
 
 export class LocalPythonExecutor implements CodeToolExecutor {
 
+    private tempDir: string| undefined ;
+    private initialized: boolean = false;
+
 
     async execte(wsPort: number, tools: AgentTool[], code: string, toolInitCallback: (tools: AgentTool[]) => void): Promise<string> {
 
-        const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'mimir-code-interpreter-'));
+        if (!this.tempDir) {
+            this.tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'mimir-code-interpreter-'));
+        }
+       
 
-        const scriptPath = path.join(tempDir, 'script.py');
+        const scriptPath = path.join(this.tempDir, 'script.py');
         const pythonScript = getPythonScript(wsPort, tools.map((t) => t.name), code);
         await fs.writeFile(scriptPath, pythonScript);
 
+        const scriptsDir = process.platform === "win32" ? 'Scripts' : 'bin';
+        const activeScriptCall = process.platform === "win32" ? `activate` : `. ./activate`;
+
 
         try {
-            console.debug(`Creating python virtual environment in ${tempDir} ...`);
-            const scriptsDir = process.platform === "win32" ? 'Scripts' : 'bin';
-            const pyenv = await executeShellCommand(`cd ${tempDir} && python -m venv .`);
-            if (pyenv.exitCode !== 0) {
-                throw new Error(`Failed to create python virtual environment: ${pyenv.output}`);
-            }
 
-            const activeScriptCall = process.platform === "win32" ? `activate` : `. ./activate`;
+            if (!this.initialized) {
+        const scriptPath = path.join(this.tempDir, 'script.py');
+        console.debug(`Creating python virtual environment in ${this.tempDir} ...`);
+             
+                const pyenv = await executeShellCommand(`cd ${this.tempDir} && python -m venv .`);
+                if (pyenv.exitCode !== 0) {
+                    throw new Error(`Failed to create python virtual environment: ${pyenv.output}`);
+                }
 
+                const externalDependencies = ["asyncio", "uvicorn", "fastapi_websocket_rpc"];
 
-            const externalDependencies = ["asyncio", "uvicorn", "fastapi_websocket_rpc"];
-
-            let libraryInstallationResult = {
-                exitCode: 0,
-                output: "",
-            }
-            for (const libraryName of externalDependencies ?? []) {
-                console.debug(`Installing library ${libraryName}...`);
-                const installationResult = await executeShellCommand(`cd ${path.join(tempDir, scriptsDir)} && ${activeScriptCall} && python -m pip install ${libraryName}`);
-                if (installationResult.exitCode !== 0) {
-                    libraryInstallationResult = {
-                        exitCode: installationResult.exitCode,
-                        output: installationResult.output + '\n------\n' + libraryInstallationResult.output,
+                let libraryInstallationResult = {
+                    exitCode: 0,
+                    output: "",
+                }
+                for (const libraryName of externalDependencies ?? []) {
+                    console.debug(`Installing library ${libraryName}...`);
+                    const installationResult = await executeShellCommand(`cd ${path.join(this.tempDir, scriptsDir)} && ${activeScriptCall} && python -m pip install ${libraryName}`);
+                    if (installationResult.exitCode !== 0) {
+                        libraryInstallationResult = {
+                            exitCode: installationResult.exitCode,
+                            output: installationResult.output + '\n------\n' + libraryInstallationResult.output,
+                        }
                     }
                 }
+                if (libraryInstallationResult?.exitCode !== 0) {
+                    console.warn(`Failed to install libraries:\n ${libraryInstallationResult?.output}`);
+                }
+                this.initialized = true;
             }
-            if (libraryInstallationResult?.exitCode !== 0) {
-                console.warn(`Failed to install libraries:\n ${libraryInstallationResult?.output}`);
-            }
-
 
             let toolInitExecuted = false;
-            const result = await executeShellCommandAndTrigger(`cd ${path.join(tempDir, scriptsDir)} && ${activeScriptCall} && python ${scriptPath}`, (data: string) => {
+            const result = await executeShellCommandAndTrigger(`cd ${path.join(this.tempDir, scriptsDir)} && ${activeScriptCall} && python ${scriptPath}`, (data: string) => {
                 if (data.includes("INITIALIZED SERVER") && !toolInitExecuted) {
                     toolInitCallback(tools);
                     toolInitExecuted = true;
